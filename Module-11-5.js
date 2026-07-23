27ed868a8a550c80ea94adcf175fba3b7672b3ba
ashrafulislam2k6 <ashraful2742k6@gmail.com>
2026-07-23 16:18:12 +0600

    Object is start

--- Module-11-5.js
+++ Module-11-5.js
@@ -1,1 +1,26 @@
 // todo :       Module no - 11-5            topic: Introduction to objects, properties and values
+
+// how to declare Object : 
+
+const product = {
+    name: 'Laptop',
+    brand: 'Apple',
+    color: 'white',
+    brandNew: true,
+    prise: 100000,
+    installApps : ['facebook', 'youtube', 'instagram', 'neptune', ]
+}
+
+
+const aboutMySelf ={
+    name: 'ashraful islam',
+    isStudent: true,
+    form:'natore',
+    institute: 'pabna polytechnic institute',
+    department : 'cst',
+    gol: 'Ai driven full stack web Engineer'
+}
+
+
+console.log(product);
+console.log(aboutMySelf);
