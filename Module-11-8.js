ed41b06f5e6d6e4a0ea72f3a94dc9a99734254b4
ashrafulislam2k6 <ashraful2742k6@gmail.com>
2026-07-23 21:27:23 +0600

    keys, values, nested object and delete

---
+++ Module-11-8.js
@@ -0,0 +1,74 @@
+// todo:        Module no : 11-8        topic:  Keys, values, nested objects and delete
+
+const country = {
+    name : 'Bangladesh',
+    primeMinister : 'Tareq jiya',
+    party: 'Bnp',
+    relativeOfPrime : 'mirza fokrul',
+
+    oppositionParty :'jamat islami',
+    headOfOppositionParty : 'jamat amir',
+    trasted : true,
+
+}
+
+
+// get keys  and values .: dote notation.
+
+
+const keys = Object.keys(country);
+console.log(keys);
+
+
+// get keys of bracket notation : 
+const allKeys = Object['keys'](country)
+console.log(allKeys);
+
+
+// get values of dote notation : 
+
+const getValues = Object.values(country);
+console.log(getValues);
+
+// get values of bracket notation : 
+
+const valuesOfBracketNotation =  Object['values'](country);
+console.log(valuesOfBracketNotation);
+
+
+// delate keyWort :  dot notation and bracket notation .
+
+delete country.party;
+console.log(country);
+
+delete country['relativeOfPrime'];
+console.log(country);
+
+
+
+// nasted object : 
+
+const  worldLeader = {
+    name : 'tramp and putin',
+    dolarCountry : 'america', 
+
+    muslimCountry : {
+        fast: 'saudi Arabia',
+        second: 'tarky',
+
+        muslimBoss : {
+            bossName : 'khamini',
+            land : 'iran',
+            power: true
+        }
+    }
+}
+
+
+console.log(worldLeader);
+
+let nasted = worldLeader.muslimCountry.muslimBoss.bossName;
+console.log(nasted);
+
+let nastedUseBracket = worldLeader['muslimCountry']['muslimBoss']['power'];
+console.log(nastedUseBracket);
