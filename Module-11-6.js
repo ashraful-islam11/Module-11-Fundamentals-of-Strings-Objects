32e6c115dfc878b8b5a93d8306e5283a0b0074be
ashrafulislam2k6 <ashraful2742k6@gmail.com>
2026-07-23 16:49:13 +0600

    object animited vedio

---
+++ Module-11-6.js
@@ -0,0 +1,30 @@
+// todo:        Module no - 11-6        topic: What is Object Animated video
+
+//  what is object and  how to declare object ; 
+ console.log('Object হলো JavaScript-এর একটি data type, যার মাধ্যমে আমরা সম্পর্কিত (related) অনেকগুলো তথ্য একসাথে একটি জায়গায় রাখতে পারি।');
+
+ let me = {
+    name: 'ashraful',
+    age: 20,
+    isStudent : true,
+    department: 'cse',
+    skills: ['html','css', 'js'],
+    profession: function (){
+        console.log('i wil be one of the best full stack web developer in bangladesh');
+    }
+ }
+
+ /**
+  * key:   name. 
+  * key:   age
+  * key:   isStudent.
+  * key:   skills
+  * key:   profession.
+  * 
+  * value: 'ashraful'
+  * value: 20. 
+  * value: true,
+  * value: 'cse'
+  * value: ['html', 'css','js']
+  * value: function(){}
+  */
---
+++ Module-11-7.js
@@ -0,0 +1,1 @@
+// todo:        Module no -         topic: 
