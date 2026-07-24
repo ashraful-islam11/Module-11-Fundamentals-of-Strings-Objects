836c8994a0d324717d45b9f70975e67ecdc89976
ashrafulislam2k6 <ashraful2742k6@gmail.com>
2026-07-24 12:26:52 +0600

    Moudul no -11 total summery

---
+++ Module-11-10.js
@@ -0,0 +1,55 @@
+// todo:    Module no - 11-10       topic: Module summery. 
+
+// * String: String-Recap: 
+
+/**
+ * todo:        string recap.
+ * 1. toLowerCase().        ===>   ছোট হাতের ।    
+ * 2. toUpperCase().         ===>  বড় হাতের ।
+ * 3. trim()                 ===>   fast and last er white space remove করতে use kora hoy.
+ * 4. trimStarts().          ===>   fast er white space removed.
+ * 5. trimEnd().             ===>   last er white space removed .
+ * 
+ * 6. slice( start, end)    ====>           slice করতে use kore hoy. কোথায় থেকে শুরু আর কার আগে শেষ হবে। 
+ * 7. concat()              ====>           একটা array er ভেতরে আরেকটা array কে সাজিয়ে ফেলে।
+ * 8. split( ' ')           ====>           string কে ভেঙ্গে Array তে রুপান্তর করে। 
+ * 9. join(' ')             ====>           array গুলকে একসাথে করে ,use করে। 
+ * 
+ * 10. includes( sub)       ====>            very vary.   নির্দিষ্ট  word আছে কিনা। true or false. 
+ * 11. startWith().         ====>            এইটা দিয়ে শুরু হয়েছে কিনা। 
+ * 12. endWith() .          ====>            এইটা দিয়ে শেষ হয়েছে কিনা। 
+ */
+
+
+//*  Object recap: 
+
+/**
+ * todo: object recap: Object হলো JavaScript-এর একটি data type, যা একাধিক সম্পর্কিত তথ্য একটি জায়গায় key-value pair  *  আকারে সংরক্ষণ করে।
+ * 
+ * 1. Object.keys(obj).     // key e get korar jonno. 
+ * 2. Object.values(obj) .  // value get korar jonno .
+ * 3. Object.entries(obj) . // 2 daimonTional , [[key: value], [key: value], [key: value]]
+ * 
+ * 4. delate.object.key.      // key ke delate korar jonno .
+ * 5. for(const key in object) .    // object e loop korar somoy for in use hoy.   array te for of .
+ * 6. object[key].                  // value ke get korar jonno. 
+ * 
+ * 7. object.key= 'value'.      //  value set korar jonno use kora hoy.
+ * 8. dot. notation.            // dot notation .
+ * 9. bracket notation .        // bracket notation.
+ * 
+ */
+
+
+
+/**
+ *  todo: Difference between String and Object in JavaScript
+ * 
+ *                  String             	            Object
+ *   1.       লেখা (text) সংরক্ষণ করে।                   একাধিক সম্পর্কিত তথ্য সংরক্ষণ করে।
+ *   2.       Primitive Data Type।                       Non-Primitive (Reference) Data Type।
+ *   3.       শুধু অক্ষর (characters) থাকে।                Key-Value Pair থাকে।
+ *   4.       Index দিয়ে access করা হয়।                   Key দিয়ে access করা হয়।
+ *   5.  Immutable (মূল value পরিবর্তন করা যায় না)।        Mutable (মূল value পরিবর্তন করা যায়)।
+ */
+
